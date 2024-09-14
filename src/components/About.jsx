7839eb312c63f0dd10c191633e3102d95bbf9a90
itzabhay01 <abhaykumar_ee21b6_19@dtu.ac.in>
2024-09-15 00:29:33 +0530

    new commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
                 initial={{opacity:0, x:100}}
                 transition={{duration: 0.5}} className='w-full lg:w-1/2'>
                     <div className='flex justify-center lg:justify-start'>
-                        <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
+                        <p className='my-2 max-w-xl py-6 text-xl'>{ABOUT_TEXT}</p>
                     </div>
                 </motion.div>
             </div>
